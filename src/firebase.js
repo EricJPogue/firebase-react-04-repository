669470e3a3f70a32ba82e7f221cb162e64e776ba
Eric Pogue <epogue@epogue.com>
2026-03-23 10:16:11 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, connectFirestoreEmulator  } from "firebase/firestore";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDdbKsLlJJBQInCFlmWF_fshSoDKDxKf4s",
+  authDomain: "fir-react-04-project.firebaseapp.com",
+  projectId: "fir-react-04-project",
+  storageBucket: "fir-react-04-project.firebasestorage.app",
+  messagingSenderId: "875501344341",
+  appId: "1:875501344341:web:1a97dc01f9afdb8e8fe6dc"
 };
 
 const app = initializeApp(firebaseConfig);
